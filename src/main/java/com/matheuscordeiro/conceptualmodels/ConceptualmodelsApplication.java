96f41621524c74009a4fe5f0f2d3e07df9225f38
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2019-11-24 17:26:34 -0300

    project created

---
+++ src/main/java/com/matheuscordeiro/conceptualmodels/ConceptualmodelsApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.conceptualmodels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConceptualmodelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConceptualmodelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheuscordeiro/conceptualmodels/ConceptualmodelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.conceptualmodels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConceptualmodelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
