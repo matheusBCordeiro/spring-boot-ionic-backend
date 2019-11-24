c6ec7404ed996333ef2f930bb82de7f2787b9c2f
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2019-11-24 17:59:57 -0300

    REST ok

---
+++ src/main/java/com/matheuscordeiro/conceptualmodels/resources/CategoryResource.java
@@ -0,0 +1,15 @@
+package com.matheuscordeiro.conceptualmodels.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoryResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String testRest() {
+		return "REST OK";
+	}
+}
