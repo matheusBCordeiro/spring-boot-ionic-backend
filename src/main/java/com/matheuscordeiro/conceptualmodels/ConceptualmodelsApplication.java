73140b91df79bc65ed1100b2ccbc499a29bc4204
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2019-11-26 22:58:56 -0300

    Creating instantiation operation

--- src/main/java/com/matheuscordeiro/conceptualmodels/ConceptualmodelsApplication.java
+++ src/main/java/com/matheuscordeiro/conceptualmodels/ConceptualmodelsApplication.java
@@ -1,13 +1,32 @@
 package com.matheuscordeiro.conceptualmodels;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.matheuscordeiro.conceptualmodels.domain.Category;
+import com.matheuscordeiro.conceptualmodels.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class ConceptualmodelsApplication {
+public class ConceptualmodelsApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConceptualmodelsApplication.class, args);
+	}
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
