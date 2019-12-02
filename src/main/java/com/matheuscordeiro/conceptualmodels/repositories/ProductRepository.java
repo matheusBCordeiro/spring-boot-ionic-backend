7c945edf09c843dfb31d87e8e9cda750edab3b00
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2019-12-01 21:55:56 -0300

    Product search by name and categories

--- src/main/java/com/matheuscordeiro/conceptualmodels/repositories/ProductRepository.java
+++ src/main/java/com/matheuscordeiro/conceptualmodels/repositories/ProductRepository.java
@@ -1,11 +1,26 @@
 package com.matheuscordeiro.conceptualmodels.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.matheuscordeiro.conceptualmodels.domain.Category;
 import com.matheuscordeiro.conceptualmodels.domain.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer>{
+	
 
+	@Transactional(readOnly=true)
+	Page<Product> findDistinctByNomeContainingAndCategoriasIn(String name, List<Category> categories, Pageable pageRequest);
+	
+	/*
+	 * Alternative Solution
+	 * @Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cat WHERE obj.name LIKE %:name% AND cat IN :categories")
+		Page<Product> search(@Param ("name") String name, @Param ("category") List<Category> categorias, Pageable pageRequest);
+	*/
 }
