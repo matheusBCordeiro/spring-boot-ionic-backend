67babf7659ef60860d31fc37c50897d396fc29c8
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2019-12-12 00:10:17 -0300

    Authorizing endpoints for specific profiles

--- src/main/java/com/matheuscordeiro/conceptualmodels/resources/CategoryResource.java
+++ src/main/java/com/matheuscordeiro/conceptualmodels/resources/CategoryResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,51 +28,52 @@ public class CategoryResource {
 
 	@Autowired
 	private CategoryService service;
-	
-	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Category> find(@PathVariable Integer id) {
 		Category object = service.find(id);
 		return ResponseEntity.ok().body(object);
 	}
-	
-	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<Void> insert(@Valid @RequestBody CategoryDTO objectDto){
+
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoryDTO objectDto) {
 		Category object = service.fromDTO(objectDto);
 		object = service.insert(object);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}").buildAndExpand(object.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(object.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
-	@RequestMapping(value="/{id}",method=RequestMethod.PUT)
+
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@RequestBody CategoryDTO objectDto, @PathVariable Integer id) {
 		Category object = service.fromDTO(objectDto);
 		object.setId(id);
 		object = service.update(object);
 		return ResponseEntity.noContent().build();
 	}
-	
-	@RequestMapping(value="/{id}",method=RequestMethod.DELETE)
+
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Category> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 
-	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<List<CategoryDTO>> findAll() {
-		List <Category> list = service.findAll();
-		List<CategoryDTO> listDto = list.stream().map(object -> new CategoryDTO(object)).collect(Collectors.toList());  
+		List<Category> list = service.findAll();
+		List<CategoryDTO> listDto = list.stream().map(object -> new CategoryDTO(object)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
-	
-	@RequestMapping(value="/page", method=RequestMethod.GET)
-	public ResponseEntity<Page<CategoryDTO>> findPage(
-			@RequestParam(value="page", defaultValue="0") Integer page, 
-			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
-			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
-			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoryDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
 		Page<Category> list = service.findPage(page, linesPerPage, orderBy, direction);
-		Page<CategoryDTO> listDto = list.map(obj -> new CategoryDTO(obj));  
+		Page<CategoryDTO> listDto = list.map(obj -> new CategoryDTO(obj));
 		return ResponseEntity.ok().body(listDto);
 	}
 }
