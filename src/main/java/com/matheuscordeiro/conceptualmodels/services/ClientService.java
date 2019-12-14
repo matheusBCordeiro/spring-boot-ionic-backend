760ff18fadff52e4c0032dac524e6b286647ce46
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2019-12-14 00:21:26 -0300

    Adjusting picture

--- src/main/java/com/matheuscordeiro/conceptualmodels/services/ClientService.java
+++ src/main/java/com/matheuscordeiro/conceptualmodels/services/ClientService.java
@@ -50,6 +50,9 @@ public class ClientService {
 
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
+	
+	@Value("${img.profile.size}")
+	private Integer size;
 
 	public Client find(Integer id) {
 
@@ -131,6 +134,8 @@ public class ClientService {
 		}
 
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
 		String fileName = prefix + user.getId() + ".jpg";
 
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
